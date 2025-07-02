28f5eefb216bdcee8741d52cad8acd8ee18bf936
MOOMIN\music <music378world@gmail.com>
2025-07-02 19:06:03 +0900

    ・usersテーブルとsubjectsテーブルのリレーションを定義

--- app/Models/Users/User.php
+++ app/Models/Users/User.php
@@ -67,7 +67,7 @@ class User extends Authenticatable
     }
 
     public function subjects(){
-        return ;// リレーションの定義
+        return $this->belongsToMany(Users::class, 'subject_users', 'user_id', 'subject_id');// リレーションの定義
     }
 
     // いいねしているかどうか
